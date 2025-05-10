bb5631268c9293b3fdda2a0062cf500aaecc7baf
Giovana <giovana.kassime.tds2023@gmail.com>
2025-05-10 20:09:15 -0300

    finalizado

--- catalogo.php
+++ catalogo.php
@@ -3,20 +3,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Catalogo</title>
+    <title>Catálogo</title>
     <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
-
+    <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
 </head>
-<body>
-    <h1>Catálogo</h1>
-    <?php 
-        ini_set('display_errors', 1);
-        error_reporting(E_ALL);
-        ini_set('display_startup_erros',1);
-        require_once 'models/Produto.php';
-        $produto = new Produto($_POST['nome_produto'], $_POST['preco_produto'], $_POST['imagem_produto'], $_POST['descricao_produto'], $_POST['avaliacao_produto']);
-        echo $produto->geraCard();
-    ?>
+<body class="bg-gray-100">
+    <header class="bg-gray-800 text-white py-4">
+        <h1 class="text-3xl font-bold text-center">Catálogo de Produtos</h1>
+    </header>
+
+    <main class="container mx-auto py-8">
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 w-full px-6">
+            <?php 
+                ini_set('display_errors', 1);
+                error_reporting(E_ALL);
+                ini_set('display_startup_erros', 1);
+                require_once 'models/Produto.php';
+
+                $produto = new Produto($_POST['nome_produto'], $_POST['preco_produto'], $_POST['imagem_produto'], $_POST['descricao_produto'], (int)$_POST['avaliacao']);
+                echo $produto->geraCard();
+            ?>
+        </div>
+    </main>
+
+
     <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
 </body>
 </html>
--- models/Produto.php
+++ models/Produto.php
@@ -16,27 +16,124 @@ class Produto
         $this->descricao = $descricao;
         $this->avaliacao = $avaliacao;
     }
-    public function geraCard()
-    {
-        return "<div class=\"w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700\">
-    <a href=\"#\">
-        <img class=\"p-8 rounded-t-lg\" src=\"{$this->imagem}\" alt=\"product image\" />
-    </a>
-    <div class=\"px-5 pb-5\">
-        <a href=\"#\">
-            <h5 class=\"text-xl font-semibold tracking-tight text-gray-900 dark:text-white\">{$this->nome}</h5>
-        </a>
-        <div class=\"flex items-center mt-2.5 mb-5\">
-            <div class=\"flex items-center space-x-1 rtl:space-x-reverse\">
-                
+    public function geraCard(){
+        return "
+        <div class=\"w-full max-w-sm bg-gray-700 border border-gray-400 rounded-lg shadow-lg\">
+            <a href=\"#\">
+                <img class=\"w-full rounded-t-lg\" src=\"{$this->imagem}\" alt=\"product image\" />
+            </a>
+            <div class=\"px-5 pb-5\">
+                <h5 class=\"text-2xl font-semibold tracking-tight text-white\">{$this->nome}</h5>
+                <p class=\"my-4 text-gray-200 break-words whitespace-normal\">{$this->descricao}</p>
+                <div class=\"flex items-center mt-2.5 mb-5\">
+                    <span class=\"flex space-x-1 text-2xl cursor-pointer\">{$this->geraEstrelasCard()}</span>
+                </div>
+                <div class=\"flex items-center justify-between\">
+                    <span class=\"text-3xl font-bold text-white\">R\${$this->preco}</span>
+                </div>
             </div>
-            <span class=\"bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded-sm dark:bg-blue-200 dark:text-blue-800 ms-3\">{$this->avaliacao}</span>
-        </div>
-        <div class=\"flex items-center justify-between\">
-            <span class=\"text-3xl font-bold text-gray-900 dark:text-white\">R\${$this->preco}</span>
-            <a href=\"#\" class=\"text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800\">Add to cart</a>
-        </div>
-    </div>
-    </div>";
+        </div>";
+    }
+    private function geraEstrelasCard(){
+    $estrelas = "";
+    $avaliacao = $this->avaliacao;
+    for ($i = 1; $i <= 5; $i++) {
+        if ($i <= $avaliacao) {
+            $estrelas .= "<i class=\"fas fa-star text-yellow-400\"></i>";
+        } else {
+            $estrelas .= "<i class=\"fas fa-star text-gray-400\"></i>";
+        }
+    }
+    return $estrelas;
+    }
+
+    /**
+     * Get the value of nome
+     */
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set the value of nome
+     */
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of preco
+     */
+    public function getPreco(): float
+    {
+        return $this->preco;
+    }
+
+    /**
+     * Set the value of preco
+     */
+    public function setPreco(float $preco): self
+    {
+        $this->preco = $preco;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of imagem
+     */
+    public function getImagem(): string
+    {
+        return $this->imagem;
+    }
+
+    /**
+     * Set the value of imagem
+     */
+    public function setImagem(string $imagem): self
+    {
+        $this->imagem = $imagem;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of descricao
+     */
+    public function getDescricao(): string
+    {
+        return $this->descricao;
+    }
+
+    /**
+     * Set the value of descricao
+     */
+    public function setDescricao(string $descricao): self
+    {
+        $this->descricao = $descricao;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of avaliacao
+     */
+    public function getAvaliacao(): int
+    {
+        return $this->avaliacao;
+    }
+
+    /**
+     * Set the value of avaliacao
+     */
+    public function setAvaliacao(int $avaliacao): self
+    {
+        $this->avaliacao = $avaliacao;
+
+        return $this;
     }
 }
